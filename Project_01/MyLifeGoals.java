3ac73883083b38bba7ace26201b6ff7bd8774a57
Jonathan Leviner <31011792+jbleviner@users.noreply.github.com>
2024-11-11 15:25:32 -0600

    Add files via upload

---
+++ Project_01/MyLifeGoals.java
@@ -0,0 +1,31 @@
+/**
+   This program prints my short term, medium term
+   and long term life goals.
+         
+   Project 1
+   @author Jonathan Leviner – COMP 1210 - 008
+   @version 8/23/2022
+*/
+
+public class MyLifeGoals
+{
+   /**
+      Prints four statements, including my name and
+      my short, medium, and long term goals.
+      @param args Command line arguments – not used.
+   */
+   public static void main(String[] args)
+   {
+      System.out.println("Jonathan Leviner");
+      System.out.println();
+      System.out.println("My short-term life goal is to maintain a good GPA "
+            + "at Auburn University and possibly finish my CO-OP work "
+            + "experience while still attending school.");
+      System.out.println("My medium-term life goal is to graduate from "
+            + "Auburn University and find a job that will help me stay "
+            + "flexible while I find a better place to settle down. ");
+      System.out.println("My long-term life goal is to help people through "
+            + "my work, build a home for myself and my family, and to "
+            + "explore more of the world and create lasting memories.");
+   }
+}
